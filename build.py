cfa5175a551ddda277120f20b9d9f14314cdf11c
Maurits Wilke <mauritswilke@gmail.com>
2025-02-26 17:56:33 +0100

    feat: add build util

---
+++ build.py
@@ -0,0 +1,38 @@
+# Inspired by @MDLC01 => https://github.com/MDLC01/board-n-pieces/blob/main/build.py
+
+#!/usr/bin/env python3
+
+from pathlib import Path
+import shutil
+import subprocess
+
+TYPST = 'typst'
+LIBRARY_DIR = Path('src/')
+PLUGIN_DIR = Path('plugin/')
+README = 'README.md'
+PLUGIN_NAME = 'mandelbrot_plugin.wasm'
+
+def build_plugin():
+    print("Building plugin")
+
+    BUILD_TARGET = 'wasm32-unknown-unknown'
+    PLUGIN_PATH = PLUGIN_DIR.joinpath('target', BUILD_TARGET, 'release', PLUGIN_NAME)
+
+    subprocess.run(
+        ['cargo', 'build', '--release', '--target', BUILD_TARGET, '--quiet'],
+        cwd=PLUGIN_DIR,
+        check=True,
+    )
+
+    shutil.copyfile(PLUGIN_PATH, LIBRARY_DIR.joinpath(PLUGIN_NAME))
+
+
+def main():
+    build_plugin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except subprocess.CalledProcessError:
+        exit(1)
